1ac88292d51f5a98df73c6cd71d3539e1d03d0b6
Cameron Sinko <csinko@gmail.com>
2017-02-25 22:12:40 -0500

    Working up command

--- serial-data/serial_data.py
+++ serial-data/serial_data.py
@@ -1,1 +1,35 @@
 ## Serial Data Testing File
+import serial
+import time
+
+ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=19200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.EIGHTBITS,
+        xonxoff=serial.XOFF,
+        rtscts=False,
+        dsrdtr=False
+        )
+
+ser.isOpen()
+print("Initializing Serial")
+print("Write Command")
+ser.write(bytearray([253,254,65,4,239]))
+time.sleep(.05)
+ser.write(bytearray([253,254,17,8,199]))
+time.sleep(.05)
+ser.write(bytearray([253,254,33,8,62]))
+time.sleep(.05)
+ser.write(bytearray([253,254,21,6,165,174,2,0,193]))
+time.sleep(.05)
+ser.write(bytearray([253,254,37,6,82,87,1,0,108]))
+time.sleep(.05)
+ser.write(bytearray([253,254,17,1,248]))
+time.sleep(.3)
+#ser.write(bytearray([253,254,35,4,20,188,199]))
+time.sleep(.05)
+ser.write(bytearray([253,254,35,2,0,1,131]))
+print("Done")
+

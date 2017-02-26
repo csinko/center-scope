e85374aa137003c786cd564ba383c620e939d0e4
Chris Paxton <crpaxton4@gmail.com>
2017-02-26 12:32:00 -0500

    Updates working version

--- python-image/CameraStream.py
+++ python-image/CameraStream.py
@@ -7,6 +7,7 @@ import imutils
 from Tkinter import *
 from PIL import Image, ImageTk
 import ttk
+import serial
 
 ### IMPORTANT VARIABLES ###
 
@@ -15,11 +16,38 @@ HEIGHT_PX = 480
 WIDTH_SEC = 26.9
 HEIHT_SEC = 20.2
 STEP = 10
-THRESHOLD = 10
-cap = cv2.VideoCapture() # 0=back camera; 1=front camera; 2=webcam
+THRESHOLD = 25
+cap = cv2.VideoCapture(2) # 0=back camera; 1=front camera; 2=webcam
 
-cap.open(2)
-print("Camera Opened")
+ser = serial.Serial(
+        port='COM4',
+        baudrate=19200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.EIGHTBITS,
+        #xonxoff=serial.XOFF,
+        rtscts=False,
+        #dsrdtr=False
+        )
+
+ser.isOpen()
+print("Initializing Serial")
+print("Write Command")
+ser.write(bytearray([253,254,65,4,239]))
+time.sleep(.05)
+ser.write(bytearray([253,254,17,8,199]))
+time.sleep(.05)
+ser.write(bytearray([253,254,33,8,62]))
+time.sleep(.05)
+ser.write(bytearray([253,254,21,6,165,174,2,0,193]))
+time.sleep(.05)
+ser.write(bytearray([253,254,37,6,82,87,1,0,108]))
+time.sleep(.05)
+ser.write(bytearray([253,254,17,1,248]))
+time.sleep(.3)
+#ser.write(bytearray([253,254,35,4,20,188,199]))
+time.sleep(.05)
+
 ##################################################
 
 ### FUNCTIONS ###
@@ -39,17 +67,19 @@ def within_threshold((x, y)):
 
 ### FIND ALL POSSIBLE STARS ###
 print 'FIND STARS'
-
+time.sleep(3)
 retv, img = cap.read()
 
-while(not retv):
+while(img is None):
     time.sleep(0.5)
     retv, img = cap.read()
+
+cv2.imshow('img', img)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #blurred = cv2.GaussianBlur(gray, (11, 11), 0)
 
-thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)[1]
 
 thresh = cv2.dilate(thresh, None, iterations=4)
 
@@ -72,7 +102,7 @@ for label in np.unique(labels):
 
 cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-cnts = contours.sort_contours(cnts)[0]
+#cnts = contours.sort_contours(cnts)[0]
 
 count = 1
 for (i, c) in enumerate(cnts):
@@ -112,11 +142,17 @@ star_x, star_y, star_w, star_h = (None, None, None, None)
 def on_click():
     global star_x, star_y, star_w, star_h
 
+
     selected_star_cnt = cnts[int(cboCombo.get())-1] # (i, c) touple [see below]
+    print("Countours")
+    print(selected_star_cnt)
     star_x, star_y, star_w, star_h = cv2.boundingRect(selected_star_cnt)
+    print("starx, stary")
+    print(star_x, star_y)
 
     root.destroy()
-    print selected_star
+    print("Selected Star")
+    print selected_star_cnt
 
 b = Button(root, text=" CONFIRM ", command=on_click)
 
@@ -138,54 +174,92 @@ print 'END DISPLAY INTERFACE'
 
 ### TELESCOPE MOVEMENT METHODS ###
 
-def start_up():
-    print 'up'
-
 def start_down():
-    print 'down'
 
-def start_right():
-    print 'right'
+    ## UP COMMAND
+    ser.write(bytearray([253,254,35,2,0,1,131]))
+    time.sleep(0.05)
+
+def start_up():
+
+    ## DOWN COMMAND
+    ser.write(bytearray([253,254,35,2,255,255,160]))
+    time.sleep(0.05)
 
 def start_left():
-    print 'left'
+
+    ## RIGHT COMMAND
+    ser.write(bytearray([253,254,19,2,0,1,42]))
+    time.sleep(0.05)
+
+def start_right():
+
+    ## LEFT COMMAND
+    ser.write(bytearray([253,254,19,2,255,255,9]))
+    time.sleep(0.05)
 
 def stop_RL():
+    #Left/Right Stop Command
+    ser.write(bytearray([253,254,19,2,0,0,45]))
+    time.sleep(0.05)
     print 'Stopping RL motor'
 
 def stop_UD():
+    #Up/Down Stop Command
+    ser.write(bytearray([253,254,35,2,0,0,132]))
+    time.sleep(0.05)
     print 'Stopping UD motor'
-
 ####################################################
 
 ### MONITOR MOVEMENT OF TELESCOPE ###
 
 #star_x, star_y, star_w, star_h
 
-center_x_min = (WIDTH_PX - THRESHOLD)/2
-center_x_max = (WIDTH_PX + THRESHOLD)/2
+center_x_min = (WIDTH_PX - THRESHOLD)/4
+center_x_max = (WIDTH_PX + THRESHOLD)/4
 
-center_y_min = (HEIGHT_PX - THRESHOLD)/2
-center_y_max = (HEIGHT_PX + THRESHOLD)/2
+center_y_min = (HEIGHT_PX - THRESHOLD)/4
+center_y_max = (HEIGHT_PX + THRESHOLD)/4
 
+
+lr_move = False
+ud_move = False
+print 'star_x, star_y'
+print star_x, star_y
+
+print 'center x min, center x max'
+print center_x_min, center_x_max
+
+print 'center y min, center y max'
+print center_y_min, center_y_max
 
 if star_x < center_x_min:
-    start_left()
-
-elif star_X > center_x_max:
+    #start_left()
+    print 'right'
     start_right()
+    lr_move = True
+
+elif star_x > center_x_max:
+    #start_right()
+    print 'left'
+    start_left()
+    lr_move = True
 
 
 if star_y < center_y_min:
+    #start_down()
+    print 'down'
     start_down()
+    ud_move = True
 
 elif star_y > center_y_max:
+    #start_up()
+    print 'up'
     start_up()
-
+    ud_move = True
 
 
 maxMean = None
-
 
 
 while(True):
@@ -197,10 +271,13 @@ while(True):
     if(frame is None):
         # if something wonky happens with the crappy camera
         # wait for it to settle
-        time.sleep(10)
+        time.sleep(1)
     else:
 
         image = frame.copy()
+
+        #print("Star x, stary")
+        #print(star_x,star_y)
         cv2.rectangle(image, (star_x, star_y), (star_x + star_w, star_y + star_h), (0, 255, 0), 2)
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -209,9 +286,8 @@ while(True):
 
         thresh = cv2.dilate(thresh, None, iterations=4)
 
-        #### NEED TO CHANGE THIS ########################
-        for y in range(star_y - star_h, star_y + star_h, 2):
-            for x in range(star_x - star_w, star_x + star_w, 2):
+        for y in range(star_y - star_h, star_y + star_h, 5):
+            for x in range(star_x - star_w, star_x + star_w, 5):
                 mask = np.zeros(thresh.shape, dtype = 'uint8')
                 cv2.circle(mask, (x, y), 10, 255, -1)
                 mean = cv2.mean(thresh, mask = mask)[0]
@@ -220,18 +296,40 @@ while(True):
                 if maxMean is None or mean > maxMean[0]:
                     if (star_x - (star_w) <= x <= star_x + (star_w)) and (star_y - (star_h) <= y <= star_y + (star_h)):
                         maxMean = (mean, (x, y))
-        ##################################################
 
         star_x, star_y = maxMean[1]
 
-        cv2.circle(image, maxMean[1], STEP, (150, 150, 255), 2)
-        cv2.rectangle(image, ((WIDTH_PX-THRESHOLD)/2, (HEIGHT_PX-THRESHOLD)/2), ((WIDTH_PX+THRESHOLD)/2, (HEIGHT_PX+THRESHOLD)/2), (0, 255, 0))
-        cv2.imshow('frame', image)
+        if center_x_min < star_x < center_x_max:
+            print 'stop LR'
+            stop_RL()
+            lr_move = False
 
+        if center_y_min < star_y < center_y_max:
+            print 'stop UD'
+            stop_UD()
+            ud_move = False
+
+
+
+
+
+        cv2.circle(image, maxMean[1], STEP, (150, 150, 255), 2)
+
+        center_thresh_x_min = (WIDTH_PX - THRESHOLD)/2
+        center_thersh_x_max = (WIDTH_PX + THRESHOLD)/2
+        center_thresh_y_min = (HEIGHT_PX - THRESHOLD)/2
+        center_thresh_y_max = (HEIGHT_PX + THRESHOLD)/2
+
+        cv2.rectangle(image, (center_x_min, center_y_min), (center_x_max, center_y_max), (0, 255, 0))
+        cv2.imshow('frame', image)
 
 
     if within_threshold(maxMean[1]):
         print("WITHIN THRESHOLD")
+        break
+
+    if not(ud_move or lr_move):
+        print 'NOT MOVING'
         break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -248,5 +346,8 @@ while(True):
 
 print("DONE!")
 
+stop_UD()
+stop_RL()
 cap.release()
 cv2.destroyAllWindows()
+ser.close()
